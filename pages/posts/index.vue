6701be267d7e774501d5f49d6faa01783c20a02b
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-10-21 22:18:43 +0700

    Creating the main sections / pages

---
+++ pages/posts/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="posts-page"></section>
+</template>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
