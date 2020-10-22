8892adc9d2020f06feaeab75463fc0543b8e5e4e
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-10-22 11:54:51 +0700

    Adding an error layout (page)

---
+++ layouts/error.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="error-page">
+    <h1>Oops, something went wrong!</h1>
+    <p>Back to <a href="/">safety</a>!</p>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  head: {
+    bodyAttrs: {
+      class: 'error-layout',
+    },
+  },
+})
+</script>
+
+<style scoped>
+.error-layout .error-page {
+  text-align: center;
+}
+
+.error-layout .error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-layout .error-page a:hover,
+.error-layout .error-page a:active {
+  color: salmon;
+}
+</style>
